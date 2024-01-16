cdb4f5d6ac19f3524ebbf9f1591c8a0e0cdfd305
Cassidy <calowe@unc.edu>
2024-01-16 13:00:59 -0500

    tryna center the silly little photo

--- src/components/recruitment.js
+++ src/components/recruitment.js
@@ -72,7 +72,7 @@ export default function Recruitment() {
                     {/*<div className="mission-col">*/}
                     {/*    <img src="pictures/beta_omicron.jpg" alt="Beta Omicron Sign"></img>*/}
                     {/*</div>*/}
-                    <div className="rectangular-column-container">
+                    <div className="container">
                         {/*changed from col^*/}
                                         
                         <img src="pictures/PSPAKRush24Spring.jpg" align="absmiddle" alt="Spring 2024 Recruitment Schedule"></img>
