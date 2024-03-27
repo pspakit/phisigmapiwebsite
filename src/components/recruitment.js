d68ce4c3bfc9b3faf6d4df1541ef0e376e51dec6
Cassidy <calowe@unc.edu>
2024-03-27 07:17:20 -0400

    taking out extra

--- src/components/recruitment.js
+++ src/components/recruitment.js
@@ -232,16 +232,11 @@ export default function Recruitment() {
                     <h1 className="has-text-centered">Recruitment Materials</h1>
                 </section>
 
-                <section className="container">
-                    <iframe title="Spring 2024 Recruitment Fact Sheet" src="https://docs.google.com/document/d/1eiCmVNGOxVHB6y841hx8X3HhLFxO7g_aBvub6A8Av_M/edit?embedded=true" scrolling="auto" width="100%" height="700"></iframe>
-                </section>
-
                 <section className="email-container">
                     <h4 className="has-text-centered"><a href="https://docs.google.com/document/d/e/2PACX-1vQmDsxMuC_st5ivG1OmzgON1VXKRt-_QGDotPQZIa5OvFDtXY35jUTqhHwHvAjcliDvE8kEhjjLcHpj/pub?embedded=true" target="_blank" rel="noreferrer">Spring Recruitment 2024 - Fact Sheet</a></h4>
                 </section>
-
                 <section className="container">
-                    <iframe title="Fall 2022 Recruitment Interest Form" src="https://docs.google.com/forms/d/e/1FAIpQLSf9AJzHUxm5TS68_eExA4rP6WqyHBZID1-LamKjsSZnssSHEg/viewform?embedded=true" width="100%" height="700">Loading…</iframe>
+                    <iframe title="Spring 2024 Recruitment Fact Sheet" src="https://docs.google.com/document/d/1eiCmVNGOxVHB6y841hx8X3HhLFxO7g_aBvub6A8Av_M/edit?embedded=true" scrolling="auto" width="100%" height="700"></iframe>
                 </section>
 
                 <section className="footer-container">
